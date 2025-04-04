699452c30e4ab161f89ba55dc2116c8b8c4fe014
Maurizio Cirino <76854252+mauriziocirino@users.noreply.github.com>
2025-04-04 13:29:22 +0200

    Add files via upload

--- sqlCode/crochetique.sql
+++ sqlCode/crochetique.sql
@@ -4,58 +4,31 @@ CREATE DATABASE IF NOT EXISTS Crochetique;
 use Crochetique;
 
 -- tabella cliente
-CREATE TABLE Cliente (
-	id int auto_increment primary key,
+CREATE TABLE Utente (
+	id int auto_increment PRIMARY KEY,
     username varchar(50) not null unique,
     pass varchar(255) not null,
     ruolo enum('admin', 'user') not null,
-    email varchar(100) not null unique,
-    dataCreazione timestamp default current_timestamp
+    email varchar(100) not null unique
 );
-
 
 -- tabella pagamento
 CREATE TABLE Pagamento(
-	numCarta varchar(19) primary key,
+	numCarta varchar(19),
+    idUtente int not null,
     nomeIntestatario varchar(30) not null,
     cognomeIntestatario varchar(30) not null,
     meseScadenza tinyint not null,
-    annoScadenza YEAR not null
-   -- CHECK (annoScadenza > YEAR(curdate()) or annoScadenza=YEAR(CURDATE()) and meseScadenza>MONTH(curdate()))
+    annoScadenza YEAR not null,
+    PRIMARY KEY (idUtente, numCarta),
+    FOREIGN KEY (idUtente)  REFERENCES Utente(id) on delete cascade
 );
-
--- tabella catalogo
-CREATE TABLE Catalogo(
-	id int auto_increment primary key,
-    nome varchar(30) not null,
-    stagione varchar(30) not null,
-    sconto int default 0,
-    CHECK (sconto between 0 and 100)
+-- tabella tessuto + colore 
+CREATE TABLE Tessuto (
+    id int auto_increment primary key,
+    materiale varchar(50) not null,
+    coloreHEX varchar(7) not null CHECK (coloreHEX REGEXP('^#[0-9A-Fa-f]{6}'))
 );
-
--- tabella articolo 
-CREATE TABLE Articolo(
-	id int auto_increment primary key,
-    descrizione varchar(100) not null, 
-    numAcquisti int default 0,
-    dimensione enum('XS','S', 'M', 'L', 'XL'),
-	price decimal(9,2) not null,
-    codiceTessuto int REFERENCES Tessuto(codice) on delete set null
-);
-
--- tabella Borsa
-CREATE TABLE Borsa(
-	id int primary key REFERENCES Articolo(id) on delete cascade, 
-    imbottitura varchar(30),
-    idModello int REFERENCES Modello(id) on delete cascade
-);
-
--- tabella Accessorio
-CREATE TABLE Accessorio(
-	id int primary key REFERENCES Articolo(id) on delete cascade,
-    indossabile boolean default 0
-);
-
 -- tabella modello
 CREATE TABLE Modello(
 	id int auto_increment primary key,
@@ -63,73 +36,64 @@ CREATE TABLE Modello(
     nome varchar(30) not null
 );
 
--- tabella tessuto + colore per non avere doppioni di colori(possono essere su tessuti diversi)
-CREATE TABLE Tessuto (
-    id int auto_increment primary key,
-    materiale varchar(50) not null,
-    idColore char(6) REFERENCES Colore(id) on delete cascade
-);
-CREATE TABLE Colore(
+-- tabella catalogo
+CREATE TABLE Catalogo(
 	id int auto_increment primary key,
-    nome varchar(30) NOT NULL UNIQUE,
-    codiceHEX char(6) CHECK (codiceHEX REGEXP('^#[0-9A-Fa-F]{6}$'))
+    nome varchar(30) not null,
+    stagione enum('Estate', 'Autunno', 'Inverno', 'Primavera') not null,
+    anno int not null
+);
+
+-- tabella articolo 
+CREATE TABLE Articolo(
+	id int auto_increment primary key,
+    descrizione varchar(100) not null, 
+    numAcquisti int default 0,
+    foto longblob default null,
+    dimensione enum('XS','S', 'M', 'L', 'XL'),
+	price decimal(9,2) not null,
+    codiceTessuto int not null,
+    idCatalogo int,
+	FOREIGN KEY (codiceTessuto) REFERENCES Tessuto(id),
+    FOREIGN KEY (idCatalogo) REFERENCES Catalogo(id) on delete set null
+);
+
+-- tabella Borsa
+CREATE TABLE Borsa(
+	id int not null, 
+    imbottitura bool default 0,
+    idModello int not null,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id) REFERENCES Articolo(id) on delete cascade,
+    FOREIGN KEY(idModello)REFERENCES Modello(id) on delete cascade
+);
+
+-- tabella Accessorio
+CREATE TABLE Accessorio(
+	id int not null,
+    indossabile tinyint default 0,
+	PRIMARY KEY(id),
+	FOREIGN KEY(id) REFERENCES Articolo(id) on delete cascade
 );
 
 -- ordine
 CREATE TABLE Ordine (
 	id int auto_increment primary key,
-    idCliente int references Cliente(id) on delete cascade,
-    sconto int default 0,
-    CHECK (sconto BETWEEN 0 AND 99),
+    idUtente int not null,
     via varchar(30) not null,
     civico varchar(6) not null,
-    CAP varchar(5) not null
+    CAP varchar(5) not null,
+    stato enum('Pagato', 'In Elaborazione', 'Spedito', 'Consegnato', 'Completato') not null,
+    FOREIGN KEY (idUtente) REFERENCES Utente(id)
 );
 
 -- tabella articoloOrdine
 CREATE TABLE articoloOrdine(
-	idArticolo int REFERENCES Articolo(id) on delete cascade,
-    idOrdine int REFERENCES Ordine(id) on delete cascade,
+	idArticolo int not null,
+    idOrdine int not null,
     quantity int default 1 not null,
-    prezzoTot decimal(10,2) default 0.00,
-    primary key (idArticolo, idOrdine)
+    totPrezzo decimal(10,2) default 0.00,
+    PRIMARY KEY (idArticolo, idOrdine),
+    FOREIGN KEY(idArticolo) REFERENCES Articolo(id),
+    FOREIGN KEY(idOrdine) REFERENCES Ordine(id) on delete cascade
 );
--- trigger per auto update numAcquisti
-DELIMITER $$
-
-CREATE TRIGGER aggiornaNumAcquisti
-AFTER INSERT ON articoloOrdine
-FOR EACH ROW
-BEGIN
-    UPDATE Articolo
-    SET numAcquisti = (
-        SELECT COALESCE(SUM(quantita), 0) --  la funzione COALESCE restituisce il primo elemento non NULL
-        FROM articoloOrdine
-        WHERE idArticolo= NEW.idArticolo
-    )
-    WHERE id = NEW.idArticolo;
-END$$
-
-DELIMITER ;
-
--- trigger update prezzo totale  (x cami : l'ho creato ma non sò nemmeno se il prof l'accetta in questo modo o lo vuole diversamente)
-DELIMITER $$
-
-CREATE TRIGGER aggiornaPrezzoTot
-BEFORE INSERT ON articoloOrdine
-FOR EACH ROW
-BEGIN
-    DECLARE prezzoUnitario decimal(10,2);
-    
-    -- Recupera il prezzo dell'articolo
-    SELECT prezzo INTO prezzoUnitario
-    FROM Articolo
-    WHERE id = NEW.idArticolo;
-    
-    -- Calcola il prezzo totale
-    SET NEW.prezzoTot = NEW.quantity * prezzoUnitario;
-END$$
-
-DELIMITER ;
-
--- SELEZIONA TUTTI I PRODOTTI CHE SONO STATI ACQUISTATI DA TUTTI I CLIENTI
